c55f63b2479266e2bf14464d2b9fd273980a7467
pau <pau.sust@gmail.com>
2024-12-28 11:01:52 +0100

    Versió inicial
    Inclou un formulari per enviar un fitxer

---
+++ smtp_params.h
@@ -0,0 +1,31 @@
+/*
+  Copyright (c) 2022 - Tőkés Attila
+
+  This file is part of SmtpClient for Qt.
+
+  SmtpClient for Qt is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  SmtpClient for Qt is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY.
+
+  See the LICENCE file for more details.
+*/
+
+#ifndef DEMO_VARS_H
+#define DEMO_VARS_H
+
+#define SMTP_SERVER "smtp.gmail.com"
+#define SMTP_PORT_SSL 465
+#define SMTP_PORT_TLS 587
+
+#define SENDER_NAME "Pau-Env"
+#define SENDER_EMAIL "pausustprofe@rovira-forns.org"
+#define SENDER_PASSWORD "rpdr thth cjij umuq"
+
+#define RECIPIENT_NAME "Pau Sust"
+#define RECIPIENT_EMAIL "pau.sust@gmail.com"
+
+#endif
